3fce560a69cd93d7ae8aa1a7896d3afa72356f03
123 <cookie.twenty01@gmail.com>
2021-05-17 14:21:36 -0400

    template added

--- user/views.py
+++ user/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(res):
-  return HttpResponse("<h1>Helloe</h1>")
+  return render(res, "user/home.html", {})
