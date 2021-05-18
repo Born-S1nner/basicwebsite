b3a441f1e81a2454a6179b0468a1019f3b9acf1e
123 <cookie.twenty01@gmail.com>
2021-05-18 14:17:48 -0400

    model and bcrypt

--- basic/user/models.py
+++ basic/user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Account(modes.Model):
+  username = models.CharField(max_length=200)
+  password = models.CharField(max_length=200)
