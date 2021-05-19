f8460f2648757b65d35d4049eb003d88212cc20a
123 <cookie.twenty01@gmail.com>
2021-05-19 14:27:01 -0400

    login change

--- basic/user/urls.py
+++ basic/user/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+  path("user/", include('django.contrib.auth.urls')),
   path("", views.home, name="home"),
   path("user/signup/", views.join, name="create"),
   path("user/login/", views.LoginReunioun, name="login"),
