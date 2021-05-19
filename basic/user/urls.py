06703f1a6456a26eb5a5caa7a5374bf98e9148fe
123 <cookie.twenty01@gmail.com>
2021-05-19 14:29:49 -0400

    remove login in views and url

--- basic/user/urls.py
+++ basic/user/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
   path("user/", include('django.contrib.auth.urls')),
   path("", views.home, name="home"),
   path("user/signup/", views.join, name="create"),
-  path("user/login/", views.LoginReunioun, name="login"),
   path("user/success/", views.Success, name="logout")
 ]
